9d827ba88aa03e86946f166906f19406c12dde33
Osama Hamed <o.hamed@fz-juelich.de>
2026-06-01 09:05:15 +0200

    Update the venv path

--- scripts/startBDA.cmd
+++ scripts/startBDA.cmd
@@ -2,7 +2,8 @@
 setlocal
 
 set "WORKINGDIRECTORY="C:\WPy64-3.13.12.0\notebooks\Dev\bda"
-set "VENV=C:\WPy64-3.13.12.0\notebooks\Envs\bigdata"
+::set "VENV=C:\WPy64-3.13.12.0\notebooks\Envs\bigdata"
+set "VENV=C:\WPy64-3.13.12.0\Envs\bigdata"
 
 cd /d "%WORKINGDIRECTORY%"
 call "%VENV%\Scripts\activate.bat"
