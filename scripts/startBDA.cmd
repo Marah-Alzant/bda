01879b591fc4d7a1324fe39f20797743e3583168
MarahAlzant <malznt@smail.ucas.edu.ps>
2026-07-02 22:25:53 +0300

    BDA Course

--- scripts/startBDA.cmd
+++ scripts/startBDA.cmd
@@ -1,9 +1,9 @@
 @echo off
 setlocal
 
-set "WORKINGDIRECTORY=D:\Opera Downloads\WPy64-31330\notebooks\Dev\bda"
-set "VENV=D:\Opera Downloads\WPy64-31330\notebooks\envs\bigdata"
-::set "VENV=D:\Opera Downloads\WPy64-31330\envs\bigdata"
+set "WORKINGDIRECTORY=C:\Users\97259\Downloads\WinPython64-3.13.13.0dot\WPy64-313130\notebooks\DEV\bda"
+set "VENV=C:\Users\97259\Downloads\WinPython64-3.13.13.0dot\WPy64-313130\notebooks\Envs\bigdata"
+
 
 cd /d "%WORKINGDIRECTORY%"
 call "%VENV%\Scripts\activate.bat"
